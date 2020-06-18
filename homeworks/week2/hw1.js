9f44ad27fa3335f8b7a3035663f0029429a4c3a0
Lily <lily@Lilyde-MacBook-Pro.local>
2020-06-18 14:52:04 +0800

    week2 完成

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,14 @@
+//給定 n（1<=n<=30），依照規律「印出」正確圖形
+
 function printStars(n) {
-  
+  if(n<1 || n>30) {
+    console.log('n is not in the range.')
+  }
+  for(i=1; i<=n; i++){
+    console.log('*')
+  }
 }
 
-printStars(5);
+//printStars(1)
+//printStars(3)
+printStars(6)
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(arr, concatStr) {
-  
+  var ansArr = arr[0]
+  for(i=1; i<=arr.length-1; i++){
+    ansArr += concatStr
+    ansArr += arr[i]
+  }
+  return ansArr
 }
 
 function repeat(str, times) {
-  
+  var ansStr = ''
+  for(j=1; j<=5; j++){
+    ansStr += str
+  }
+  return ansStr
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(['a'], '!'))
+console.log(join([1,2,3], ''))
+console.log(join(["a", "b", "c"], '!'))
+console.log(join(["a", 1, "b", 2, "c", 3], ','))
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'))
+
+console.log(repeat('a', 5))
+console.log(repeat('yoyo', 2))
